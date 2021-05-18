6f5f8cc206032afd721cedd16d339fc1712aaa77
Pablo Araújo <rafanet.live@gmail.com>
2021-05-18 00:12:58 -0300

    Adding react fresh refresh.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
@@ -14,6 +15,8 @@ if (process.env.NODE_ENV === "production") {
 module.exports = {
   mode: mode,
   target: target,
+
+  entry: "./src/index.js",
 
   output: {
     path: path.resolve(__dirname, "dist"),
@@ -53,6 +56,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./src/index.html",
     }),
+    new ReactRefreshWebpackPlugin(),
   ],
   resolve: {
     extensions: [".js", ".jsx"],
